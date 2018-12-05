cbb4354ca69c3b21229bec619836eecbfb6a41ee
Carlo Beltrame <carlo@beltra.me>
2018-12-05 16:54:17 +0100

    Add the same test for headers style API requests

---
+++ tests/GroupDetailsHeadersTest.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__.'/../BaseTest.php';
+require_once __DIR__.'/../assertions/ResponseCode200.php';
+require_once __DIR__.'/../assertions/JsonResponse.php';
+require_once __DIR__.'/../assertions/ContainsSingleGroupDetail.php';
+
+class GroupDetailsHeadersTest extends BaseTest {
+
+  public function get_name() {
+    return 'GET /groups/{id} (headers style)';
+  }
+
+  public function perform() {
+    $this->do_get_with_headers_style('/groups/' . $this->groupId);
+  }
+
+  /**
+   * @return array
+   */
+  public function get_assertions() {
+    return [
+      new ResponseCode200(),
+      new JsonResponse(),
+      new ContainsSingleGroupDetail($this->groupId),
+    ];
+  }
+}

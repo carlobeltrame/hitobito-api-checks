050ad38b3f1f324fca3222835f367ae2d5cd1bc5
Carlo Beltrame <carlo@beltra.me>
2018-12-07 09:22:52 +0100

    Security: Check that only PHP scripts in the tests directory are accessed

--- public/tests.php
+++ public/tests.php
@@ -21,14 +21,21 @@ $groupId = getParam('groupId');
 
 if ($test && $hitobitoUrl && $apiToken && $groupId) {
 
-  require_once $test;
-  $classname = basename($test, '.php');
-  /** @var BaseTest $testObject */
-  $testObject = new $classname($hitobitoUrl, $apiToken, $groupId);
-  echo json_encode($testObject->test());
+  // Check that we only access tests in the test directory
+  $testDir = realpath(__DIR__ . '/tests');
+  $testPath = realpath($test);
+  if (substr($testPath, 0, strlen($testDir)) === $testDir) {
 
-} else {
+    require_once $test;
+    $classname = basename($test, '.php');
+    /** @var BaseTest $testObject */
+    $testObject = new $classname($hitobitoUrl, $apiToken, $groupId);
+    echo json_encode($testObject->test());
 
-  echo json_encode(getClassesFromDirectory('tests'));
+    return;
+
+  }
 
 }
+
+echo json_encode(getClassesFromDirectory('tests'));

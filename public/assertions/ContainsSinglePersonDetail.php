96138fdbbc6f40828dfda28cb3d1891f18fc4552
Carlo Beltrame <carlo@beltra.me>
2018-12-09 14:53:07 +0100

    Add tests for getting a single person's details

---
+++ public/assertions/ContainsSinglePersonDetail.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once __DIR__ . '/../BaseAssertion.php';
+require_once __DIR__ . '/../BaseTest.php';
+require_once __DIR__ . '/../AssertionFailedException.php';
+
+class ContainsSinglePersonDetail extends BaseAssertion {
+
+  protected $personId;
+
+  public function __construct($personId) {
+    $this->personId = $personId;
+  }
+
+  public function __invoke(BaseTest $test) {
+
+    $json = $test->get_response_body();
+    $body = json_decode($json);
+    if (!isset($body->people)) {
+      throw new AssertionFailedException(
+        'Response body does not contain a people entry.',
+        'JSON response containing a people entry.',
+        $json
+      );
+    }
+    if (!is_array($body->people)) {
+      throw new AssertionFailedException(
+        'people entry in response body is not a list.',
+        'JSON list in people entry of response.',
+        $json
+      );
+    }
+    if (count($body->people) !== 1) {
+      throw new AssertionFailedException(
+        'people entry in response body does not contain exactly one element.',
+        'JSON list in people entry of response containing exactly one element.',
+        $json
+      );
+    }
+
+    $personDetail = $body->people[0];
+    if (!isset($personDetail->id)) {
+      throw new AssertionFailedException(
+        'Person details should contain an id entry.',
+        'JSON object containing an id entry.',
+        var_export($personDetail, true)
+      );
+    }
+    if ($personDetail->id !== $this->personId) {
+      throw new AssertionFailedException(
+        'Id inside person details didn\'t match expected value.',
+        $this->personId,
+        var_export($personDetail->id)
+      );
+    }
+  }
+}
---
+++ public/tests/PersonByIdHeadersTest.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once __DIR__ . '/../HeadersTest.php';
+require_once __DIR__ . '/../assertions/ResponseCode200.php';
+require_once __DIR__ . '/../assertions/JsonResponse.php';
+require_once __DIR__ . '/../assertions/ContainsSinglePersonDetail.php';
+
+class PersonByIdHeadersTest extends HeadersTest {
+
+  protected $personGroupId;
+  protected $personId;
+
+  public function get_name() {
+    return 'Get a person by id (' . parent::get_name() . ')';
+  }
+
+  protected function find_any_person($groupId) {
+    $this->do_get_request('/groups/' . $groupId . '/people');
+    $body = json_decode($this->get_response_body());
+    if (isset($body->people) && is_array($body->people) && count($body->people)) {
+      return array($groupId, $body->people[0]->id);
+    }
+
+    // Try to find a person in a subgroup
+    $this->do_get_request('/groups/' . $groupId);
+    $body = json_decode($this->get_response_body());
+    if (isset($body->groups) && is_array($body->groups) && count($body->groups)) {
+      foreach ($body->groups as $subgroup) {
+        list($groupIdWithPerson, $personId) = $this->find_any_person($subgroup->id);
+        if ($groupIdWithPerson !== null && $personId !== null) {
+          return array($groupIdWithPerson, $personId);
+        }
+      }
+    }
+
+    // No person found, give up
+    return array(null, null);
+  }
+
+  public function given() {
+    parent::given();
+
+    list($this->personGroupId, $this->personId) = $this->find_any_person($this->groupId);
+
+    if ($this->personGroupId === null || $this->personId === null) {
+      throw new TestNotApplicableException('No person found that is accessible with this token.');
+    }
+  }
+
+  public function when() {
+    $this->do_get_request('/groups/' . $this->personGroupId . '/people/' . $this->personId);
+  }
+
+  public function then() {
+    return [
+      new ResponseCode200(),
+      new JsonResponse(),
+      new ContainsSinglePersonDetail($this->personId),
+    ];
+  }
+}
